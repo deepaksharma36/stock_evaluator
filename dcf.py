bdc5aaa596efe96611c1a949081c49833bd0a71f
Deepak Sharma <deepak.jiitn@gmail.com>
2019-12-13 23:22:15 -0500

    Fixed discounted growth rate bug

--- dcf.py
+++ dcf.py
@@ -3,7 +3,7 @@ def simple_dcf_value(cur_cash_flow, growth, discount_rate, period,
                      growth_discount_rate=10):
     dcf_value = 0
     for year_count in range(period):
-        growth = growth*(.90)**period
+        growth = growth*(.90)**year_count
         profit = cur_cash_flow*(1 + growth/100)**year_count
         discount = (1 + discount_rate)**year_count
         dcf_value += profit/discount
